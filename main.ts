8c9b8419dd6ace7a8204df822a1550c07f0c9605
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-10-20 20:31:03 -0400

    L38

--- main.ts
+++ main.ts
@@ -5,4 +5,35 @@
  * This program plays rock paper scissers
 */
 
+let randomNumber : number = -1
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function ()
+{
+    randomNumber = randint(0, 2)
+    basic.clearScreen()
+    
+    if (randomNumber == 0) {
+        basic.showIcon(IconNames.Square)
+    }
+
+    if (randomNumber == 1) {
+        
+        basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    }
+
+    if (randomNumber == 2) {
+        basic.showIcon(IconNames.Scissors)
+    }
+
+    basic.pause(5000)
+    basic.showIcon(IconNames.Happy)
+})
 
